6e11800f8a9fe2b3fe996165bc1ad859e8cc2a8b
Michal Orzechowski <orzechowski.michal@gmail.com>
2016-03-14 12:26:35 +0100

    Update run.sh

--- scenario5/run.sh
+++ scenario5/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # This script hs to be run from the directory where docker-compose.yml is located
+# To exit this scenario and stop all onedata components use CTRL-C in the terminal. 
 
 source ../env
 docker-compose up
