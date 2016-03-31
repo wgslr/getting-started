c2895dd8deaeaa4a2f57c283d4f5ae6864a7b7b2
Michal Orzechowski <orzechowski.michal@gmail.com>
2016-04-01 01:43:55 +0200

    made the script more user friendly, help info etc.

--- run.sh
+++ run.sh
@@ -1,63 +1,85 @@
 #!/bin/bash
 # POSIX
 
-# To exit this scenario and stop all onedata components use CTRL-C in the terminal. 
+# To exit this scenario and stop all onedata components use CTRL-C in the terminal.
 
 # Reset all variables that might be set
 . ./env.sh
 
-# Usage info
-show_help() {
-cat << EOF
-Usage: ${0##*/} [-hv] [-f OUTFILE] [FILE]...
-Do stuff with FILE and write the result to standard output. With no FILE
-or when FILE is -, read standard input.
-
-    -h, --help       display this help and exit
-    -s, --scenario   run specified scenario
-EOF
+# Error handling.
+# $1 - error string
+die() {
+  echo "${0##*/}: error: $*" >&2
+  exit 1
 }
 
-n=0
-service=""
+# As the name suggests
+usage() {
+  echo "Usage: ${0##*/}  [-h] [ -se <service>] -s <number>
 
-while [[ $# > 0 ]]
-do
-    case $1 in
-        -h|-\?|--help)   # Call a "show_help" function to display a synopsis, then exit.
-            show_help
-            exit
-            ;;
-        --scenario)       # Takes an option argument, ensuring it has been specified.
-            if [ -n "$2" ]; then
-                n=$2
-                shift
-            else
-                printf 'ERROR: "--scenario" requires a non-empty option argument.\n' >&2
-                exit 1
-            fi
-            ;;
-        --service)       # Takes an option argument, ensuring it has been specified.
-            if [ -n "$2" ]; then
-                service=$2
-                shift
-            else
-                printf 'ERROR: "--service" requires a non-empty option argument.\n' >&2
-                exit 1
-            fi
-            ;;
-        -?*)
-            printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
-            ;;
-        *)               # Default case: If no more options then break out of the loop.
-            forward_args="$forward_args $1"
-    esac
-    shift
-done
+This script starts specified Onedata scenarios from directories ./scenario{N}
 
-if [ "$service" != "" ]; then
+Example usage:
+${0##*/} -s 1 -se oneprovider
+
+Options:
+  -h, --help       display this help and exit
+  -s, --scenario   run specified scenario
+  -se, --service   run specyfic service from the scenario"
+  exit 0
+}
+
+
+main() {
+  local n=0
+  local service
+
+  while [[ $# > 0 ]]; do
+      case $1 in
+          -h|-\?|--help)   # Call a "usage" function to display a synopsis, then exit.
+              usage
+              exit
+              ;;
+          --scenario)       # specify scenari number
+              if [ -n "$2" ]; then
+                  n=$2
+                  shift
+              else
+                  printf 'ERROR: "--scenario" requires a non-empty option argument.\n' >&2
+                  exit 1
+              fi
+              ;;
+          --service)       # specify service name
+              if [ -n "$2" ]; then
+                  service=$2
+                  shift
+              else
+                  printf 'ERROR: "--service" requires a non-empty option argument.\n' >&2
+                  exit 1
+              fi
+              ;;
+          -?*)
+              printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+              ;;
+          *)               # Default case: If no more options then break out of the loop.
+              forward_args="$forward_args $1"
+      esac
+      shift
+  done
+
+  if [[ -z ${n+x} ]]; then
+    die "-s flag missing, please specify the scenario number "
+  fi
+
+  if [[ -z ${service+x} ]]; then
     cd "scenario$n" && docker-compose up "$service"
-else
+  else
     cd "scenario$n" && docker-compose up
+  fi
+}
+
+if [ $# -lt 1 ]; then
+    usage
 fi
 
+main "$@"
