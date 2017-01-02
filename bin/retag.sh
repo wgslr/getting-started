1843fb9c12d505364befee7340d657e04ba65ac8
Michal Orzechowski <orzechowski.michal@gmail.com>
2017-01-02 17:29:30 +0100

    first version of retag.sh, script form regating scenarions and publishing docker images

---
+++ bin/retag.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+
+# This script is used to update tags in compose files across scenarios and to publish images
+
+publish() {
+	local from="$1"
+	local to="$2"
+	docker pull "$from"
+	docker tag "$from" "$to"
+	docker push "$to"
+}
+
+retag() {
+	local component_name="$1"
+	local full_name="$2"
+	echo "Displaying all old tags for $component_name and runing test sed to see if they get replaced by $full_name"
+	find . -type f -iname "docker-compose-*.yml" -exec grep -aHb --color -E "image:\s+\w+/$component_name:.*"  {} \;
+	find . -type f -iname "docker-compose-*.yml" -exec sed  -n "s#image:\s\+\(\w\+\)/$component_name:.*#image: $full_name#gp" {} \;
+
+	echo "Retaring and displaying changes"
+	find . -type f -iname "docker-compose-*.yml" -exec sed -i "s#image:\s\+\(\w\+\)/$component_name:.*#image: $full_name#g" {} \;
+	find . -type f -iname "docker-compose-*.yml" -exec grep -aHb --color -E "image:\s+$full_name"  {} \;
+}
+
+usage() {
+	  echo "TODO"
+	  exit 0
+}
+
+main() {
+
+  if (( ! $# )); then
+    usage
+  fi
+
+  while (( $# )); do
+      case $1 in
+          -h|-\?|--help)   # Call a "usage" function to display a synopsis, then exit.
+              usage
+              exit 0
+              ;;
+          --oz)
+              OZ_FROM="$2"
+              shift
+              ;;
+          --oz-to)
+              OZ_TO="$2"
+              shift
+              ;;
+          --op)
+              OP_FROM="$2"
+              shift
+              ;;
+          --op-to)
+              OP_TO="$2"
+              shift
+              ;;
+          --oc)
+              OC_FROM="$2"
+              shift
+              ;;
+          --oc-to)
+              OC_TO="$2"
+              shift
+              ;;
+          --retag)
+              RETAG=1
+              ;;
+          --nn)
+              NIGHTLY=1
+              ;;
+           -?*)
+              printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+              exit 1
+              ;;
+          *)
+              die "no option $1"
+              ;;
+      esac
+      shift
+  done
+
+  if [[ $NIGHTLY -eq 1 ]]; then
+    [[ ! -z "$OZ_FROM" ]] && OZ_TO="onedata/onezone:nightly-$OZ_FROM" && OZ_FROM="docker.onedata.org/onezone:$OZ_FROM"  
+    [[ ! -z "$OP_FROM" ]] && OP_TO="onedata/oneprovider:nightly-$OP_FROM" && OP_FROM="docker.onedata.org/oneprovider:$OP_FROM" 
+    [[ ! -z "$OC_FROM" ]] && OC_TO="onedata/oneclient:nightly-$OC_FROM" && OC_FROM="docker.onedata.org/oneclient:$OC_FROM" 
+  fi 
+
+  echo "$OC_FROM"
+  [[ ! -z "$OZ_FROM" ]] && [[ ! -z "$OZ_TO" ]] && publish "$OZ_FROM" "$OZ_TO"
+  [[ ! -z "$OP_FROM" ]] && [[ ! -z "$OP_TO" ]] && publish "$OP_FROM" "$OP_TO"
+  [[ ! -z "$OC_FROM" ]] && [[ ! -z "$OC_TO" ]] && publish "$OC_FROM" "$OC_TO"
+
+  if [[ $RETAG -eq 1 ]]; then
+    [[ ! -z "$OZ_TO" ]] && retag onezone "$OZ_TO"
+    [[ ! -z "$OP_TO" ]] && retag oneprovider "$OP_TO"
+    [[ ! -z "$OC_TO" ]] && retag oneclient "$OC_TO"
+
+  fi
+}
+
+main "$@"
