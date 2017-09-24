8a398aa8f5c4bea3f3ab174f16661601b2fd0eca
Michal Orzechowski <orzechowski.michal@gmail.com>
2017-09-24 15:04:56 +0200

    copy config script now only manages letsencrypt

--- scenarios/0_0_egi_datahub/copy_config.sh
+++ scenarios/0_0_egi_datahub/copy_config.sh
@@ -2,5 +2,5 @@
 
 _HOSTS="onedata01.cloud.plgrid.pl zonedb01.cloud.plgrid.pl zonedb02.cloud.plgrid.pl"
 for _HOST in $_HOSTS; do
-  echo sudo rsync -crlvz -e ssh --stats --progress -e "ssh -i /home/ubuntu/.ssh/id_rsa"  --rsync-path="sudo rsync" /volumes/* ubuntu@${_HOST}:/volumes
+  sudo rsync -crlvz -e ssh --stats --progress -e "ssh -i /home/ubuntu/.ssh/id_rsa"  --rsync-path="sudo rsync" /etc/letsencrypt ubuntu@${_HOST}:/etc/
 done
