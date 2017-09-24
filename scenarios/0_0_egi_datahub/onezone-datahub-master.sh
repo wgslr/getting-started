3c230683492aef0a48d69d18c5446fbe17e102f1
Michal Orzechowski <orzechowski.michal@gmail.com>
2017-09-24 15:08:17 +0200

    added missing source command to start scripts

--- scenarios/0_0_egi_datahub/onezone-datahub-master.sh
+++ scenarios/0_0_egi_datahub/onezone-datahub-master.sh
@@ -2,4 +2,6 @@
 YAML_FILE=docker-compose-datahub-zone-master.yml
 PROJECT_NAME=datahub-onezone-master
 
+source onezone-datahub-lib.sh
+
 main "$@"
--- scenarios/0_0_egi_datahub/onezone-datahub-worker.sh
+++ scenarios/0_0_egi_datahub/onezone-datahub-worker.sh
@@ -2,4 +2,6 @@
 YAML_FILE=docker-compose-datahub-zone-worker.yml
 PROJECT_NAME=datahub-onezone-worker
 
+source onezone-datahub-lib.sh
+
 main "$@"
