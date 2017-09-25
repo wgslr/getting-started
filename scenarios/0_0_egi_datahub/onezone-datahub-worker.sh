6cfa4791dde0b8a2b438529df882a604f4a8638d
Michal Orzechowski <orzechowski.michal@gmail.com>
2017-09-25 21:28:08 +0200

    unified naming of onezone, zone->onezone

--- scenarios/0_0_egi_datahub/onezone-datahub-worker.sh
+++ scenarios/0_0_egi_datahub/onezone-datahub-worker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-YAML_FILE=docker-compose-datahub-zone-worker.yml
+YAML_FILE=docker-compose-datahub-onezone-worker.yml
 PROJECT_NAME=datahub-onezone-worker
 
 source onezone-datahub-lib.sh
