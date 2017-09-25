ddafb0b1c98b271dee9013f469b2ad4545a9c478
Michal Orzechowski <orzechowski.michal@gmail.com>
2017-09-25 14:11:31 +0200

    minor configuration fixed for egi oneprovider

--- scenarios/0_0_egi_datahub_oneprovider/attach-to-all-nodes.sh
+++ scenarios/0_0_egi_datahub_oneprovider/attach-to-all-nodes.sh
@@ -8,8 +8,8 @@ tmux new-session -s "$session" \
     split-window -d '/usr/bin/env bash -l'\; \
     split-window -d '/usr/bin/env bash -l'\; \
     select-layout tiled\; \
-	send-keys -t "${pane}.1" C-z 'ssh ubuntu@datahub.plgrid.pl' Enter\; \
-	send-keys -t "${pane}.2" C-z 'ssh ubuntu@datahubdb01.plgrid.pl' Enter\; \
-	send-keys -t "${pane}.3" C-z 'ssh ubuntu@datahubdb02.plgrid.pl' Enter\; \
+	send-keys -t "${pane}.0" C-z 'ssh ubuntu@datahub.plgrid.pl' Enter\; \
+	send-keys -t "${pane}.1" C-z 'ssh ubuntu@datahubdb01.plgrid.pl' Enter\; \
+	send-keys -t "${pane}.2	" C-z 'ssh ubuntu@datahubdb02.plgrid.pl' Enter\; \
 	bind b set-window-option synchronize-panes\; \
-    attach-session%
+    attach-session
